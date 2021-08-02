b34deb304adf2350c9e9cd6478ce339801dbf899
Droma1 <nando.rex333@gmail.com>
2021-08-01 21:05:33 -0500

    2 push

--- Config/Config_general.php
+++ Config/Config_general.php
@@ -1,5 +1,5 @@
 <?php
-	const SERVERURL="http://192.168.0.6/web/Colegio/";
+	const SERVERURL="http://192.168.0.10/web/Colegio/";
 	const COMPANY="I.E. Sr. de los Milagros";
 	date_default_timezone_set("America/Bogota");
 	setlocale(LC_TIME, "spanish");
