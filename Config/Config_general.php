49de929eacb84ed69b7b5b6d57cdecc04bf5ffe2
Droma1 <nando.rex333@gmail.com>
2021-08-03 21:30:32 -0500

    vc, 2.4.1

--- Config/Config_general.php
+++ Config/Config_general.php
@@ -1,5 +1,7 @@
 <?php
 	const SERVERURL="http://192.168.0.10/web/Colegio/";
+		//direccion de mi servidor
+		//localhost/colegio/
 	const COMPANY="I.E. Sr. de los Milagros";
 	date_default_timezone_set("America/Bogota");
 	setlocale(LC_TIME, "spanish");
